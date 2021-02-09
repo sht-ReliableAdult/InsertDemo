10fda176f4b6f799dc8f8765dfdeebe920a687c9
sht-ReliableAdult <lzmoqf@163.com>
2021-02-09 14:01:12 +0800

    verify bytecode api

--- myplugin/src/main/java/com/example/myplugin/InsertTransform.java
+++ myplugin/src/main/java/com/example/myplugin/InsertTransform.java
@@ -21,16 +21,21 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
+import org.apache.tools.ant.taskdefs.Local;
 import org.apache.tools.ant.taskdefs.Mkdir;
+import org.apache.tools.ant.taskdefs.SubAnt;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 
+import org.objectweb.asm.Type;
 import org.objectweb.asm.commons.AdviceAdapter;
+import org.objectweb.asm.commons.Method;
 
 /**
  * 继承Gradle安卓版插件提供的Transform
@@ -102,6 +107,7 @@ public class InsertTransform extends Transform {
                 File file = directoryInput.getFile();
                 //DFS获取目录下的所有class文件
                 List<File> classFiles = getClassFormDirectory(file, new ArrayList<File>());
+                //生成一段加密标识符，可以与之前的比对看类是否调整过
                 String md5Name = DigestUtils.md5Hex(file.getAbsolutePath());
                 //获取provider提供的对应中转目录
                 File fileOutputDirectory = outputProvider.getContentLocation(name + md5Name, directoryInput.getContentTypes()
@@ -153,33 +159,39 @@ public class InsertTransform extends Transform {
      * 插桩逻辑
      */
     private void doInsert(File classFile, File fileOutput) throws IOException {
-        FileInputStream inputStream = new FileInputStream(classFile);
-        //这里可以传byte数组或者IOStream
-        ClassReader cr = new ClassReader(inputStream);
-        //代表class自动计算栈帧和局部变量表大小，其他模式需要自己计算
-        ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
-        //执行分析修改,需要传一个传版本号和ClassWriter的ClassVisitor和另一个参数
-        cr.accept(new MyClassVisitor(Opcodes.ASM5, cw), ClassReader.EXPAND_FRAMES);
-        //将修改结果转成byte数组覆盖原class
-        byte[] resultClass = cw.toByteArray();
-        FileOutputStream outputStream = new FileOutputStream(fileOutput);
-        outputStream.write(resultClass);
-        inputStream.close();
-        outputStream.close();
+        if (classFile.getName().contains("Fragment") || classFile.getName().contains("Application")) {
+            FileUtils.copyFile(classFile, fileOutput);
+        }else {
+            FileInputStream inputStream = new FileInputStream(classFile);
+            //这里可以传byte数组或者IOStream
+            ClassReader cr = new ClassReader(inputStream);
+            //代表class自动计算栈帧和局部变量表大小，其他模式需要自己计算
+            ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+            //执行分析修改,需要传一个传版本号和ClassWriter的ClassVisitor和另一个参数
+            cr.accept(new MyClassVisitor(Opcodes.ASM5, cw), ClassReader.EXPAND_FRAMES);
+            //将修改结果转成byte数组覆盖原class
+            byte[] resultClass = cw.toByteArray();
+            FileOutputStream outputStream = new FileOutputStream(fileOutput);
+            outputStream.write(resultClass);
+            inputStream.close();
+            outputStream.close();
+        }
+
     }
 
     static class MyClassVisitor extends ClassVisitor {
 
         public MyClassVisitor(int i, ClassVisitor classVisitor) {
+
             super(i, classVisitor);
         }
         //访问到某一个类的方法，只能处理方法声明，要处理方法体需要MethodVisitor
         @Override
         public MethodVisitor visitMethod(int i, String s, String s1, String s2, String[] strings) {
             System.out.println(s);
-            //如果是onCreate方法，就返回带有修改逻辑的MethodVisitor，其他方法返回默认什么也不改的MethodVisitor
+            //如果是打印日志方法，就返回带有修改逻辑的MethodVisitor，其他方法返回默认什么也不改的MethodVisitor
             MethodVisitor methodVisitor = super.visitMethod(i, s, s1, s2, strings);
-            if (s.equals("onCreate")) {
+            if (s.equals("exportLogInfo")) {
                 return new MyMethodVisitor(this.api, methodVisitor, i, s, s1);
             }
             return methodVisitor;
@@ -197,38 +209,62 @@ public class InsertTransform extends Transform {
             @Override
             protected void onMethodEnter() {
                 super.onMethodEnter();
-                //插入lang l = System.currentTimeMillis();
+                //插入Lang l = System.currentTimeMillis();
+                /*
+                //1.执行System.currentTimeMillis()
+                invokeStatic(Type.getType("Ljava/lang/System;"),new Method("currentTimeMills", "()J"));
+                //2.1创建一个Long类型本地变量，在本处代号为s
+                s = newLocal(Type.LONG_TYPE);
+                //2.2用代号s的本地变量接收执行结果
+                storeLocal(s);
+                 */
             }
+            int s;
             //马上退出方法
             @Override
             public void monitorExit() {
                 super.monitorExit();
                 //插入long e = System.currentTimeMillis();
                 //System.out.println("excute" + (e - l) + "ms.");
+                /*
+                invokeStatic(Type.getType("Ljava/lang/System;"),new Method("currentTimeMills", "()J"));
+                int e = newLocal(Type.LONG_TYPE);
+                storeLocal(e);
+                //执行sout静态反法
+                getStatic(Type.getType("Ljava/lang/System;"), "out", Type.getType("Ljava/io/PrintStream;"));
+                //new StringBuilder
+                newInstance(Type.getType("Ljava/lang/StringBuilder;"));
+                dup();
+                //执行SB构造方法
+                invokeConstructor(Type.getType("Ljava/long/StringBuilder;"),new Method("<init>", "()V"));
+                //添加字符串excute
+                visitLdcInsn("excute:");
+                //执行SB的append方法
+                invokeVirtual(Type.getType("Ljava/long/StringBuilder;"),new Method("append", "(Ljava/long/String;)Ljava/long/StringBuilder;"));
+                //两个时间作减法
+                loadLocal(e);
+                loadLocal(s);
+                math(SUB, Type.LONG_TYPE);
+                //把Long类型的差append进去
+                invokeVirtual(Type.getType("Ljava/long/StringBuilder;"),new Method("append", "(J)Ljava/long/StringBuilder;"));
+                //添加字符串ms.
+                visitLdcInsn("ms.");
+                invokeVirtual(Type.getType("Ljava/long/StringBuilder;"),new Method("append", "(Ljava/long/String;)Ljava/long/StringBuilder;"));
+                invokeVirtual(Type.getType("Ljava/long/StringBuilder;"),new Method("toString", "()Ljava/long/String;"));
+                invokeVirtual(Type.getType("Ljava/io/PrintStream;"),new Method("println", "(Ljava/long/String;)V"));
+                 */
             }
             //这里可以实现删除指定方法内的某方法
             @Override
             public void visitMethodInsn(int opcode, String owner, String name, String descriptor, boolean isInterface) {
                 //走到指定方法，不还原直接return
-                if (name.equals("exportLogInfo")) {
+                if (name.equals("d")) {
                     return;
                 }
                 super.visitMethodInsn(opcode, owner, name, descriptor, isInterface);
             }
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
 
     @Override
     public boolean isIncremental() {
